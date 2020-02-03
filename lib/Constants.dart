d3d81bb3cbe15bdcf7e0625d929a3d22715d14e2
anlichandra <anlichn20@gmail.com>
2020-02-03 11:06:11 +0700

    Update Constants.dart
    change server

--- lib/Constants.dart
+++ lib/Constants.dart
@@ -8,6 +8,7 @@ class Constants{
   ];
 
 
-  static const String server = 'http://192.168.42.152/kmember';
+  //static const String server = 'http://192.168.42.152/kmember';
+  static const String server = 'https://www.kasimurasupermart.com/kmember';
 
 }
